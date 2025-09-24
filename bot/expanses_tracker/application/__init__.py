c9fc8ba1f08af41982d0d308df0f7851fbd374e1
Alessandro Giacche <alessandrogiacche@gmail.com>
2025-09-24 23:35:38 +0200

    feat: add /get n command handler

--- bot/expanses_tracker/application/__init__.py
+++ bot/expanses_tracker/application/__init__.py
@@ -11,6 +11,7 @@ from expanses_tracker.application.features.delete_expense.delete_command_handler
 )
 from expanses_tracker.application.utils.decorators import ensure_access_guard
 from expanses_tracker.application.features.buttons import setup_buttons_handlers
+from expanses_tracker.application.features.get_expenses import get_command_handler
 
 log = logging.getLogger(__name__)
 
@@ -32,6 +33,7 @@ def application_registration(app):
     """Register application handlers for the Telegram bot."""
     app.add_handler(CommandHandler("start", __cmd_start__))
     app.add_handler(CommandHandler("delete", delete_command_handler))
+    app.add_handler(CommandHandler("get", get_command_handler))
     app.add_handler(MessageHandler(~filters.COMMAND, generic_message_handler), group=1)
     setup_buttons_handlers(app)
     return app
