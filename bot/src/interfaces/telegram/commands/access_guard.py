a59ed552806fa94bda27266ce0e7b5776a4809a8
Alessandro Giacche <25297222+alejack9@users.noreply.github.com>
2025-12-02 23:22:07 +0100

    fix: setting usage

--- bot/src/interfaces/telegram/commands/access_guard.py
+++ bot/src/interfaces/telegram/commands/access_guard.py
@@ -5,7 +5,7 @@ import logging
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from src.config import settings
+from src.config.settings import Settings
 
 log = logging.getLogger(__name__)
 
@@ -14,7 +14,7 @@ def ensure_access_guard(func):
     """Decorator to ensure that only authorized users can access the decorated handler."""
     async def __ensure_access(update: Update) -> bool:
         uid = update.effective_user.id if update.effective_user else 0
-        if not (not settings.allowed_chat_ids or uid in settings.allowed_chat_ids):
+        if not (not Settings.allowed_chat_ids or uid in Settings.allowed_chat_ids):
             if effective_chat := update.effective_chat:
                 await effective_chat.send_message("⛔ Unauthorized.")
             else:
